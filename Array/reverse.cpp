5865a605975e6d665f5b940fbb9f9c0d3e676151
Eakansh Bhardwaj <129897503+eaclix@users.noreply.github.com>
2024-09-01 00:38:07 +0530

    Update reverse.cpp

--- Array/reverse.cpp
+++ Array/reverse.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void reversearray(int arr[], int n, int k){
+    reverse(arr, arr+k);
+    reverse(arr+k, arr-n);
+    reverse(arr, arr+n);
+}
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the elements of array: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int k;
+    cout<<"Enter the value of k: ";
+    cin>>k;
+
+    reversearray(arr, n, k);
+    cout<<"The rotated array is: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
