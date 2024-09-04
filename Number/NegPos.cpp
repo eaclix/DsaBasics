bca756a9ebb7b60b2855f5e93e044e9a42fc30b1
Eakansh Bhardwaj <129897503+eaclix@users.noreply.github.com>
2024-09-05 01:23:30 +0530

    Create NegPos.cpp

---
+++ Number/NegPos.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    
+    if(n>0){
+        cout<<"Positive";
+    }
+    else{
+        cout<<"Negative";
+    }
+
+    return 0;
+}
