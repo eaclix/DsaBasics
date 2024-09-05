69f2efd4f26fef8a9d9cb788e34e385c44c2e88f
Eakansh Bhardwaj <129897503+eaclix@users.noreply.github.com>
2024-09-05 16:14:04 +0530

    Create greatestofTWO.cpp

---
+++ Number/greatestofTWO.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a: ";
+    cin >> a;
+
+    double b;
+    cout << "Enter b: ";
+    cin >> b;
+
+    if(a>b){
+        cout<< a;
+    }
+    else cout<< b;
+
+    return 0;
+}
