32c2daedcb1cde75a1d2e549a1a4d6e03e57c8cd
Eakansh Bhardwaj <129897503+eaclix@users.noreply.github.com>
2024-09-11 01:58:04 +0530

    Create rootsofQuadratic.cpp

---
+++ Number/rootsofQuadratic.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+    cout << "Enter the value of c: ";
+    cin >> c;
+
+    double root1, root2;
+
+    double discriminant = (b * b) - (4 * a * c);
+
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "Roots are real and different." << endl;
+        cout << "Root 1 = " << root1 << endl;
+        cout << "Root 2 = " << root2 << endl;
+    } else if (discriminant == 0) {
+        root1 = root2 = -b / (2.0 * a);
+        cout << "Roots are real and same." << endl;
+        cout << "Root 1 = Root 2 = " << root1 << endl;
+    } else {
+        double realPart = -b / (2.0 * a);
+        double imaginaryPart = sqrt(-discriminant) / (2.0 * a);
+        cout << "Roots are complex and different." << endl;
+        cout << "Root 1 = " << realPart << " + " << imaginaryPart << "i" << endl;
+        cout << "Root 2 = " << realPart << " - " << imaginaryPart << "i" << endl;
+    }
+
+    return 0;
+}
