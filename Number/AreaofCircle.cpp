7102b117b1f80ca1ef395b07cce939cb1fbff545
Eakansh Bhardwaj <129897503+eaclix@users.noreply.github.com>
2024-09-11 01:44:09 +0530

    Create AreaofCircle.cpp

---
+++ Number/AreaofCircle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int r;
+    cout<<"Enter the radius of Circle: ";
+    cin>>r;
+
+    double area = 3.14*r*r;
+    cout<<area;
+    return 0;
+
+}
